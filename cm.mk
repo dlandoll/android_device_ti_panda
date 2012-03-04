492584e5a657d983755f98cf88ab862e04a19482
Darren Landoll <darren.landoll@gmail.com>
2012-03-04 17:10:25 -0500

    Compile CM9 for Panda
    Signed-off-by: Darren Landoll <darren.landoll@gmail.com>

---
+++ cm.mk
@@ -0,0 +1,16 @@
+# Release name
+PRODUCT_RELEASE_NAME := PANDA
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/ti/panda/full_panda.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := panda
+PRODUCT_NAME := cm_panda
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Panda
+PRODUCT_MANUFACTURER := Texas_Instruments_Inc
+
